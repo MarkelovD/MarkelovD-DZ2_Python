b4f4065224168f7bbf91befb71c90bab0151fbc5
Dmitry <markelov16@gmail.com>
2022-12-21 18:43:16 +0500

    3 задача

--- DZ2.py
+++ DZ2.py
@@ -12,9 +12,22 @@
 
 # 2 задача
 #Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр.
-a = float(input("введите вещественное число: "))
-sum = 0
-for i in str(a):
-    if i != ".":
-        sum+=int(i)
-print(sum)
+# a = float(input("введите вещественное число: "))
+# sum = 0
+# for i in str(a):
+#     if i != ".":
+#         sum+=int(i)
+# print(sum)
+
+#3 задача
+# задайте список из n чисел последовательности
+# $(1+\frac 1 n)^n$ и выведите на экран их сумму.
+
+nums = int(input("введие число: "))
+sumLibrary = 0
+library = {}
+for i in range(1, nums+1):
+    library[i] = round(((1+1/i)**i),2)
+    sumLibrary += library[i]
+print(library)
+print(round(sumLibrary,2))
