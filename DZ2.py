6eb2920d068f94e800420aa581c81a9cefb2a118
Dmitry <markelov16@gmail.com>
2022-12-21 18:38:06 +0500

    2 задача

--- DZ2.py
+++ DZ2.py
@@ -1,11 +1,20 @@
 # 1 задача
 # Напишите программу, которая принимает на вход число N
 #  и выдает набор произведений чисел от 1 до N.
-nums = int(input("введите число N: "))
-numbersInList =1
-count =1
-table = []
-for i in range (1,nums+1):
-    numbersInList *=i
-    table.append(numbersInList)
-print(table)
+# nums = int(input("введите число N: "))
+# numbersInList =1
+# count =1
+# table = []
+# for i in range (1,nums+1):
+#     numbersInList *=i
+#     table.append(numbersInList)
+# print(table)
+
+# 2 задача
+#Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр.
+a = float(input("введите вещественное число: "))
+sum = 0
+for i in str(a):
+    if i != ".":
+        sum+=int(i)
+print(sum)
